2fd90f3a6b3e332bb68cca4f39a08e4910e6a813
Ageu-Wolf <ageuesilva.rieger@gmail.com>
2025-09-04 16:35:20 -0300

    Codigo dia 04-09-2025

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,include
+
 """
 URL configuration for agenda project.
 
@@ -17,10 +20,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('', include('fornecedores.urls')),
-]
+    path('', include('clientes.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
